44c432457c8dc707a897b79dfdca9dfadc5eee7d
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2026-01-31 09:53:24 +0200

    Making auth routes and auth controller with auth actions

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use Hash;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|min:6'
+        ]);
+
+        User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+
+        return redirect('/login');
+
+    }
+
+
+
+
+    public function login(Request $request){
+        if(Auth::attempt($request->only('email','password'))){
+            $request->session()->regenerate();
+            return redirect('/todos');
+        }
+
+        return back()->withErrors(['email'=>'Invalid data']);
+
+    }
+
+
+
+
+
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    }
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register',[AuthController::class,'registerForm']);
+Route::post('/register',[AuthController::class,'register']);
+
+Route::get('/login',[AuthController::class,'loginForm']);
+Route::post('/login',[AuthController::class,'login']);
+
+Route::post('logout',[AuthController::class,'logout']);
