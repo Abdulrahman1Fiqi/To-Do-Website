a3a3681589003f9f9a6947fb042abe1783e239f4
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2026-01-31 12:51:21 +0200

    init todo controller and model

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,4 +17,6 @@ Route::post('/login',[AuthController::class,'login'])->name('login');
 Route::post('logout',[AuthController::class,'logout']);
 
 
-
+Route::middleware('auth')->group(function(){
+    Route::resource('todos',TodoController::class);
+});
